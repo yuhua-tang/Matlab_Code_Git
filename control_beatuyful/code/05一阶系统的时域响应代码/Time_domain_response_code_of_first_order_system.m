a23be7ad0cf36bbb81180b8e4d87228c9d2803ff
yuhua <taniao.tang@hairobotics.com>
2022-11-16 19:50:04 +0800

    modify control_beatyful_code_class5

--- control_beatuyful/code/05一阶系统的时域响应代码/Time_domain_response_code_of_first_order_system.m
+++ control_beatuyful/code/05一阶系统的时域响应代码/Time_domain_response_code_of_first_order_system.m
@@ -2,7 +2,9 @@ clc;clear;close all;
 %% 加载Control Package,使用Matlab则注释下1行
 % pkg load control
 %% 定义一阶系统
-G_s = tf([5],[1 5]);
+num = [-1 5];
+den = [1 5 4];
+G_s = tf(num,den);
 %% 仿真
 subplot(3,1,1)
 %% 单位冲激响应
@@ -12,11 +14,15 @@ subplot(3,1,2)
 step(G_s);
 %% 对初始状态的响应
 subplot(3,1,3)
-x0 = 10;
-A = -5;
-B = -5;
-C = 1; 
-D = 0;
-sys = ss(A,B,C,D);
-initial(sys,x0);
+% x0 = 10;
+% A = -5;
+% B = -5;
+% C = 1; 
+% D = 0;
+% sys = ss(A,B,C,D);
+% initial(sys,x0);
 
+[A,B,C,D] = tf2ss(num,den)
+sys = ss(A,B,C,D);
+x0 = eye(size(B))*10
+initial(sys,x0);
